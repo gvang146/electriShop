604e8f9dfa93dd3396669b5e16d9f448aa923eca
gvang146 <gvang146@gmail.com>
2021-10-18 09:44:19 -0500

    a

--- src/components/ShoppingCart/Cart.js
+++ src/components/ShoppingCart/Cart.js
@@ -7,6 +7,7 @@ import {
     Typography,
     IconButton,
     Paper,
+    Divider
 } from "@material-ui/core";
 import React from "react";
 import ListItems from "./ListItems";
@@ -20,7 +21,11 @@ const useStyle = makeStyles({
     },
     tabSize: {
         maxWidth: 100,
-    }
+    },
+    cartTitle: {
+        display: "flex",
+        marginLeft: 150,
+    },
 })
 
 export default function Cart() {
@@ -29,20 +34,25 @@ export default function Cart() {
 
     return (
         <div>
-        <IconButton onClick={() => setState(true)}>
-           <ShoppingCartIcon />
-        </IconButton>
-        <Paper>
-            <SwipeableDrawer
-                anchor="right"
-                open={state}
-                onClose={() => setState(false)}
-                onOpen={() => setState(true)}
-                classes={{ paper: classes.paper }}
+            <IconButton onClick={() => setState(true)}>
+                <ShoppingCartIcon />
+            </IconButton>
+            <Paper>
+                <SwipeableDrawer
+                    anchor="right"
+                    open={state}
+                    onClose={() => setState(false)}
+                    onOpen={() => setState(true)}
+                    classes={{ paper: classes.paper }}
                 >
+                    <div className={classes.cartTitle}>
+                        <Typography variant="h5"> Cart </Typography>
+
+                    </div>
+                    <Divider />
                     <ListItems />
-            </SwipeableDrawer>
-        </Paper>
+                </SwipeableDrawer>
+            </Paper>
         </div>
     )
 }
